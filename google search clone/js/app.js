874d384add847c75afa15c94cd7d4b47ed1cb714
tusharmahajan <tushar.mahajan37@gmail.com>
2020-08-25 14:51:57 +0530

    added enhancements in formatting the search results

--- google search clone/js/app.js
+++ google search clone/js/app.js
@@ -1,10 +1,23 @@
 const HOST = 'server.com/';
 
 const searchInput = document.getElementsByClassName('search__bar__input')[0];
+const suggestionsElement = document.getElementsByClassName('search__suggestions__list')[0];
+const actionsElement = document.getElementsByClassName('search__actions')[0];
+
+function wrapBoldedCharacters({inputValue, suggestion}) {
+  if (suggestion.startsWith(inputValue)) {
+    return `${suggestion.substring(0, inputValue.length)}<b>${suggestion.substring(inputValue.length, suggestion.length)}</b>`;
+  }
+  return `<b>${suggestion}</b>`;
+}
 
 function createSuggestionElement({suggestion, auxiliaryData}) {
   const auxiliaryString = auxiliaryData ? ` - ${auxiliaryData}` : "";
-  return `<li class="search__suggestions__list__result">${suggestion}${auxiliaryString}</li>`
+  const boldProcessedSuggestion = wrapBoldedCharacters({
+    inputValue: searchInput.value,
+    suggestion
+  });
+  return `<li class="search__suggestions__list__result">${boldProcessedSuggestion}${auxiliaryString}</li>`
 }
 
 function onSuggestionsResponse(data) {
@@ -17,10 +30,20 @@ function onSuggestionsResponse(data) {
     });
   }
   suggestionsElement.innerHTML = suggestionsHTML;
+  if (suggestionsHTML) {
+    actionsElement.classList.add('search__actions--autosuggest');
+  } else {
+    actionsElement.classList.remove('search__actions--autosuggest');
+  }
 }
 
 function onNewInput(event) {
-  api.get(HOST + 'autocomplete', searchInput.value, onSuggestionsResponse);
+  if (searchInput.value) {
+    api.get(HOST + 'autocomplete', searchInput.value, onSuggestionsResponse);
+  } else {
+    suggestionsElement.innerHTML = '';
+    actionsElement.classList.remove('search__actions--autosuggest');
+  }
 }
 
 searchInput.oninput = onNewInput;
